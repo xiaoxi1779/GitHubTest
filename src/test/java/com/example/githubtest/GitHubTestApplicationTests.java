5985cec1946e3833e7bd3061821fd409b29f08b6
xiaoxi1779 <xiaoxi1779@126.com>
2023-08-14 10:23:33 +0800

    jenkins测试

---
+++ src/test/java/com/example/githubtest/GitHubTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
